dec7d206bb697af4ad4c119d3bc19b3a8cdecd43
Will <will.bradshaw50@gmail.com>
2023-07-15 16:53:57 +1000

    fixed fonts

--- users/will/home.nix
+++ users/will/home.nix
@@ -237,11 +237,11 @@
       anchor = bottom
       margin = 100
       border_size = 2
-      bar_padding = 0
-      border_offset = 5
+      bar_padding = 2
+      border_offset = 2
       background_color = ${config.colorScheme.colors.base01}
-      border_color = ${config.colorScheme.colors.base04}
-      bar_color = ${config.colorScheme.colors.base05}
+      border_color = ${config.colorScheme.colors.base06}
+      bar_color = ${config.colorScheme.colors.base0E}
       height = 30
     '';
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
@@ -275,18 +275,18 @@
   
   home.file = {
     ".config/fontconfig/fonts.conf".text = ''
-      <?xml version="1.0"?>
-      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-      <fontconfig>
-        <match target="pattern">
-	<test name="family" qual="any">
-	  <string>monospace</string>
-	</test>
-	<edit binding="strong" mode="prepend" name="family">
-	  <string>BlexMono Nerd Font Mono</string>
-	</edit>
-	</match>
-      </fontconfig>
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+<fontconfig>
+  <match target="pattern">
+    <test name="family" qual="any">
+      <string>monospace</string>
+    </test>
+    <edit binding="strong" mode="prepend" name="family">
+      <string>BlexMono Nerd Font Mono</string>
+    </edit>
+   </match>
+</fontconfig>
     '';
   };
 
