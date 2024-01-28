bdd52920dc3ce216b72a9ee4fd2d978d1ed9b8e7
will <will.bradshaw50@gmail.com>
2024-01-28 13:02:28 +1100

    remove redundant xdg config

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -1,16 +1,7 @@
-{
+{ pkgs, ... }: {
   services.flatpak.enable = true;
 
   fonts.fontDir.enable = true; # Enabled for greater compatability
   
   programs.hyprland.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    config = {
-      common = {
-        default = [ "gtk" ];
-      };
-    };
-  };
 }
