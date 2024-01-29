83098ac8ef888c5319698aea5ea0cf1d2964adbc
Will <will.bradshaw50@gmail.com>
2024-01-29 20:49:59 +1100

    add move window rebind and animations

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -5,7 +5,6 @@
     enable = true;
     settings = {
       misc.disable_hyprland_logo = true;
-      animations.enabled = false;
       bind = [
         "SUPER, Return, exec, alacritty"
 
@@ -14,16 +13,34 @@
         "SUPER, K, movefocus, u"
         "SUPER, L, movefocus, d"
 
-        "SUPER, H, movewindow, l"
-        "SUPER, J, movewindow, r"
-        "SUPER, K, movewindow, u"
-        "SUPER, L, movewindow, d"
+        "SUPER ALT, H, movewindow, l"
+        "SUPER ALT, J, movewindow, r"
+        "SUPER ALT, K, movewindow, u"
+        "SUPER ALT, L, movewindow, d"
+
+        "SUPER, mouse:272, movewindow"
 
         "SUPER SHIFT, Q, killactive"
 
         "SUPER, mouse_down, workspace, e+1"
         "SUPER, mouse_up, workspace, e-1"
       ] ++ (builtins.concatLists (builtins.genList (x: ["SUPER, ${toString (x+1)}, workspace, ${toString (x+1)}"]) 9)) ++ (builtins.concatLists (builtins.genList (x: ["SUPER SHIFT, ${toString (x+1)}, movetoworkspacesilent, ${toString (x+1)}"]) 9));
+
+      animations = {
+        enabled = true;
+        animation = [
+          "windows, 0"
+          "fade, 0"
+          "border, 0"
+          "borderangle, 0"
+          "workspaces, 0"
+
+          "windowsMove, 1, 3, md3_decel, popin 60%"
+        ];
+        bezier = [
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+        ];
+      };
 
       general = {
         "col.inactive_border" = "rgb(${config.colorScheme.colors.base04})";
