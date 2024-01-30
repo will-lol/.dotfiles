32534e5797341f0747953f875dbeb27affb54d82
Will <will.bradshaw50@gmail.com>
2024-01-30 12:56:03 +1100

    overlay glfw

---
+++ nixos/common/minecraft.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, ... }: {
+  nixpkgs.overlays = lib.mkAfter [
+    (final: prev: rec {
+      glfw = final.glfw-wayland-minecraft;
+
+      prismlauncher = prev.prismlauncher.override {
+        glfw = final.glfw-wayland-minecraft;
+      };
+    })
+  ];
+}
