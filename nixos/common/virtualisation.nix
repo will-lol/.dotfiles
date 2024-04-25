1a0c7f595efdb836daa97bd6df749d3c57702631
example <example@example.com>
2024-04-25 12:13:44 +1000

    make compat

--- nixos/common/virtualisation.nix
+++ nixos/common/virtualisation.nix
@@ -1,6 +1,5 @@
 {config, ...}: {
   virtualisation.libvirtd.enable = true;
   virtualisation.docker.enable = true;
-  programs.virt-manager.enable = true;
   users.users.${config.username}.extraGroups = ["libvirtd" "docker"];
 }
