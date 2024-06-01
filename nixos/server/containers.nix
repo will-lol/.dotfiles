076e0ddc241b6b120fbb1fd35f6506a160823347
will <will.bradshaw50@gmail.com>
2024-06-01 22:22:54 +1000

    dfus container

--- nixos/server/containers.nix
+++ nixos/server/containers.nix
@@ -1,7 +1,20 @@
 {pkgs, ...}: {
   virtualisation.oci-containers.containers = {
-    samba = {
-
+    dufs = {
+      ports = ["80:8080"];
+      volumes = [
+        "/mnt/store:/data"
+      ];
+      imageFile = pkgs.dockertools.buildLayeredImage {
+        name = "dufs";
+        config = {
+          Cmd = ["${pkgs.dufs}" "-A" "-b" "127.0.0.1" "-p" "8080"];
+          WorkingDir = "/data";
+          Volumes = {
+            "/data" = {};
+          };
+        };
+      };
     };
   };
 }
