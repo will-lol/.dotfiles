b5204f0675c375571bb3fa548388a2a702f72b83
will <will.bradshaw50@gmail.com>
2024-06-01 15:13:43 +1000

    fiiiiiiiiix that up

--- nixos/server/ssh.nix
+++ nixos/server/ssh.nix
@@ -5,7 +5,7 @@
     settings.KbdInteractiveAuthentication = false;
   };
   users.users.${config.username}.openssh.authorizedKeys.keyFiles = [
-    config.sops.secrets."sshkey/public".path
+    "${config.sops.secrets."sshkey/public".path}"
   ];
   # enable non interactive sudo using ssh keys
   security.pam.services.sudo.sshAgentAuth = true;
