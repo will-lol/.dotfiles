c6f6b0819825b9359521774db959663607e018af
will <will.bradshaw50@gmail.com>
2024-06-01 15:05:53 +1000

    enable ssh

--- nixos/server/ssh.nix
+++ nixos/server/ssh.nix
@@ -1,6 +1,8 @@
 {config, ...}: {
   services.openssh = {
     enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
   };
   users.users.${config.username}.openssh.authorizedKeys.keyFiles = [
     ${config.sops.secrets."sshkey/public".path}
