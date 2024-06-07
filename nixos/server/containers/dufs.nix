e059ac79d3cc11c4a09a977960df5950d6c6556b
will <will.bradshaw50@gmail.com>
2024-06-07 22:23:49 +1000

    fix it i guess

--- nixos/server/containers/dufs.nix
+++ nixos/server/containers/dufs.nix
@@ -8,7 +8,7 @@
     wantedBy = ["podman-dufs-secrets.service"];
     before = ["podman-dufs-secrets.service"];
     script = ''
-      ${pkgs.podman}/bin/podman secret rm dufs-auth
+      ${pkgs.podman}/bin/podman secret rm dufs-auth -i
       printf "admin:$(cat ${config.sops.secrets."dufs/hash".path})@/:rw|@/pub" | ${pkgs.podman}/bin/podman secret create dufs-auth -
     '';
   };
