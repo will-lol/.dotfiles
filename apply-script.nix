4de1db423f2d2008f421a7454b0f1a1227bcde16
will <will.bradshaw50@gmail.com>
2024-07-27 15:42:22 +1000

    use nom in apply script

--- apply-script.nix
+++ apply-script.nix
@@ -2,6 +2,6 @@
 pkgs.writeShellScriptBin "apply-system" ''
   pushd ~/.dotfiles
   rm ~/.mozilla/firefox/default/search.json.mozlz4
-  sudo nixos-rebuild switch --show-trace --upgrade --impure --flake ".#$1"
+  sudo nixos-rebuild switch --show-trace --upgrade --log-format internal-json -v --flake ".#$1" |& ${pkgs.nix-output-monitor}/bin/nom --json
   popd
 ''
