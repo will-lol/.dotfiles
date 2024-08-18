2be405b451fbff7702ebff7a4c56c504bd88688e
will <will.bradshaw50@gmail.com>
2024-08-18 16:53:23 +1000

    wez

--- home/modules/gui/wezterm.nix
+++ home/modules/gui/wezterm.nix
@@ -1,8 +1,9 @@
-{config}: {
+{config, ...}: {
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
     colorSchemes = {
+      default = {
         foreground = "${config.colorScheme.palette.base08}";
         background = "${config.colorScheme.palette.base00}";
 
@@ -15,29 +16,10 @@
         compose_cursor = "${config.colorScheme.palette.base0F}";
         scrollbar_thumb = "${config.colorScheme.palette.base02}";
 
-        ansi = ["${config.colorScheme.palette.base00}" "${config.colorScheme.palette.base08}" "${config.colorScheme.palette.base0B}" "${config.colorScheme.palette.base0A}" "${config.colorScheme.palette.base0D}" "${config.colorScheme.palette.base0E}" "${config.colorScheme.palette.base0C}" "${config.colorScheme.palette.base07}"];
-        regular1 = "";
-        regular2 = "${config.colorScheme.palette.base0B}";
-        regular3 = "${config.colorScheme.palette.base0A}";
-        regular4 = "${config.colorScheme.palette.base0D}";
-        regular5 = "${config.colorScheme.palette.base0E}";
-        regular6 = "${config.colorScheme.palette.base0C}";
-        regular7 = "${config.colorScheme.palette.base09}";
-        bright0 = "${config.colorScheme.palette.base03}";
-        bright1 = "${config.colorScheme.palette.base08}";
-        bright2 = "${config.colorScheme.palette.base0B}";
-        bright3 = "${config.colorScheme.palette.base0A}";
-        bright4 = "${config.colorScheme.palette.base0D}";
-        bright5 = "${config.colorScheme.palette.base0E}";
-        bright6 = "${config.colorScheme.palette.base0C}";
-        bright7 = "${config.colorScheme.palette.base07}";
-        "16" = "${config.colorScheme.palette.base05}";
-        "17" = "${config.colorScheme.palette.base0F}";
-        "18" = "${config.colorScheme.palette.base01}";
-        "19" = "${config.colorScheme.palette.base05}";
-        "20" = "${config.colorScheme.palette.base04}";
-        "21" = "${config.colorScheme.palette.base06}";
+        ansi = ["${config.colorScheme.palette.base00}" "${config.colorScheme.palette.base08}" "${config.colorScheme.palette.base0B}" "${config.colorScheme.palette.base0A}" "${config.colorScheme.palette.base0D}" "${config.colorScheme.palette.base0E}" "${config.colorScheme.palette.base0C}" "${config.colorScheme.palette.base09}"];
+        
+        brights = ["${config.colorScheme.palette.base03}" "${config.colorScheme.palette.base08}" "${config.colorScheme.palette.base0B}" "${config.colorScheme.palette.base0A}" "${config.colorScheme.palette.base0D}" "${config.colorScheme.palette.base0E}" "${config.colorScheme.palette.base0C}" "${config.colorScheme.palette.base07}"];
+      };
     };
-    
-  
+  };
 }
