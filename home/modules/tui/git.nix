407a5377b6a0b1d878046e05c7a1929e38beac6b
will <will.bradshaw50@gmail.com>
2024-08-31 15:17:09 +1000

    add signing

--- home/modules/tui/git.nix
+++ home/modules/tui/git.nix
@@ -1,14 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   home.packages = with pkgs; [ pass ];
 
   programs.git = {
     enable = true;
     userEmail = "will.bradshaw50@gmail.com";
     userName = "will";
+    signing = {
+      signByDefault = true;
+      key = "${config.sops.secrets."sshkey/private".path}";
+    };
     extraConfig = {
       credential.helper =
         "${pkgs.git-credential-manager}/bin/git-credential-manager";
       init.defaultBranch = "main";
+      gpg.format = "ssh";
     };
   };
 }
